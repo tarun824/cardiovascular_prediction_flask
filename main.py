f30483887f21d6be171699eb9f75a12ed316184b
tarun824 <tarun.rs82004@example.com>
2024-11-21 12:54:48 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+app=Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+
+@app.route("/predict_disease",methods= ["POST"])
+def predict_disease():
+    try:
+        data = request.get_json()
+        # age=request.args.get("age")
+        # gender=request.args.get("gender")
+        # height=request.args.get("height")
+        # weight=request.args.get("weight")
+        # # ap_hi=request.args.get("ap_hi")
+        # # ap_lo=request.args.get("ap_lo")
+        # smoke=request.args.get("smoke")
+        # alco=request.args.get("alco")
+
+        age=data.get("age")
+        gender=data.get("gender")
+        height=data.get("height")
+        weight=data.get("weight")
+        ap_hi=data.get("ap_hi")
+        ap_lo=data.get("ap_lo")
+        smoke=data.get("smoke")
+        alco=data   .get("alco")
+        new_data = [[age, gender, height, weight, 100, 60, smoke, alco]]
+        print(new_data)
+        #load the model and predict the data
+        #create a new data
+        # model = joblib.load(open("../cardiovascular_prediction/model/cardiovascular_prediction_model.plk", 'rb'))
+        # scaler = joblib.load(open("../cardiovascular_prediction/model/cardiovascular_prediction_scaler.plk", 'rb'))
+
+        model = joblib.load(open("models/cardiovascular_prediction_model.plk", 'rb'))
+        scaler = joblib.load(open("models/cardiovascular_prediction_scaler.plk", 'rb'))
+        # model = pickle.load(open("../model/cardiovascular_prediction_model.plk", 'rb'))
+        age =age*365
+
+        # new_data = [[7474, 1, 156, 56, 100, 60, 0, 0]]
+        new_data = [[age, gender, height, weight, ap_hi, ap_lo, smoke, alco]]
+        print(new_data)
+        new_data = scaler.transform(new_data)
+        result = model.predict(new_data)
+        print(jsonify({"status":1,'prediction': result.tolist()[0]}))
+        return jsonify({"status":1,'prediction': result.tolist()[0]})
+    except Exception as e:
+        return jsonify({"status":0,"error":str(e)})
+
+# def get
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
